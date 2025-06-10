146cc425643f4f1500a65558b734b0cf98e214a2
Соколов Егор ТСА-143 <egorsokolov810@gmail.com>
2025-06-10 10:05:12 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -68,5 +68,5 @@ void checkValue(const double value)
 
 double calculateDistance(const double x1,const double y1,const double x2,const double y2)
 {
-    return sqrt(pow(((x2 - x1)^2) + pow((y2 - y1)^2));
+    return sqrt(pow((x2 - x1, 2) + pow(y2 - y1, 2));
 }
