66b38eec2459f60ff01c56341ff273f85a8b412e
Соколов Егор ТСА-143 <egorsokolov810@gmail.com>
2025-06-17 11:31:51 +0300

    Update Task5.cpp

--- Task5.cpp
+++ Task5.cpp
@@ -1,141 +1,238 @@
 #include <iostream>
-#include <cstdlib> // для rand() и malloc/free
-#include <ctime>   // для time()
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-// Функция для вывода массива
-void printMatrix(int** matrix, int n, int m) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            cout << matrix[i][j] << "\t";
+/**
+ * @enum SELECT
+ * @brief Перечисление для выбора способа заполнения массива
+ */
+enum SELECT
+{
+    RANDOM = 0,    ///< Автоматическое заполнение случайными числами
+    MANUALLY = 1   ///< Ручное заполнение с клавиатуры
+};
+
+/**
+ * @brief Проверяет корректность размера массива
+ * @param n Проверяемый размер
+ * @throw std::invalid_argument Если размер некорректен (n <= 0)
+ */
+void checkN(const int n);
+
+/**
+ * @brief Получает от пользователя размер массива
+ * @return Размер массива в беззнаковом типе
+ */
+size_t getSize();
+
+/**
+ * @brief Считывает целое число с клавиатуры
+ * @return Введенное число
+ * @throw std::runtime_error При ошибке ввода
+ */
+int getNumber();
+
+/**
+ * @brief Выводит двумерный массив на экран
+ * @param arr Константная ссылка на выводимый массив
+ */
+void printArray(const vector<vector<int>>& arr);
+
+/**
+ * @brief Заполняет массив случайными числами в заданном диапазоне
+ * @param arr Ссылка на заполняемый массив
+ * @param min Нижняя граница диапазона
+ * @param max Верхняя граница диапазона
+ */
+void fillArrayRandom(vector<vector<int>>& arr, const int min, const int max);
+
+/**
+ * @brief Проверяет корректность диапазона значений
+ * @param min Нижняя граница диапазона
+ * @param max Верхняя граница диапазона
+ * @throw std::invalid_argument Если min > max
+ */
+void checkRange(const int min, const int max);
+
+/**
+ * @brief Заполняет массив вручную с клавиатуры
+ * @param arr Ссылка на заполняемый массив
+ * @param min Нижняя граница допустимых значений
+ * @param max Верхняя граница допустимых значений
+ */
+void fillArray(vector<vector<int>>& arr, const int min, const int max);
+
+/**
+ * @brief Заменяет максимальный элемент каждой строки на ноль
+ * @param arr Ссылка на преобразуемый массив
+ */
+void replaceMaxWithZero(vector<vector<int>>& arr);
+
+/**
+ * @brief Вставляет строку из нулей перед строками, где первый элемент делится на 3
+ * @param arr Ссылка на преобразуемый массив
+ */
+void insertZeroRows(vector<vector<int>>& arr);
+
+/**
+ * @brief Точка входа в программу
+ * @return EXIT_SUCCESS при успешном выполнении, EXIT_FAILURE при ошибке
+ */
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите количество строк и столбцов массива: ";
+    int rows = getSize();
+    int cols = getSize();
+    
+    vector<vector<int>> arr(rows, vector<int>(cols));
+
+    cout << "Введите минимальное и максимальное значение диапазона: ";
+    int minValue = getNumber();
+    int maxValue = getNumber();
+    checkRange(minValue, maxValue);
+
+    cout << "Введите выбор для заполнения массива: " << endl
+        << RANDOM << " - Для случайного заполнения" << endl
+        << MANUALLY << " - Для ручного заполнения" << endl;
+
+    int choice = getNumber();
+
+    switch ((SELECT)choice)
+    {
+    case RANDOM:
+        fillArrayRandom(arr, minValue, maxValue);
+        break;
+    case MANUALLY:
+        fillArray(arr, minValue, maxValue);
+        break;
+    default:
+        cout << "Ваш выбор неверен" << endl;
+        return -1;
+    }
+
+    cout << "\nИсходный массив:\n";
+    printArray(arr);
+
+    replaceMaxWithZero(arr);
+    cout << "\nМассив после замены максимальных элементов строк на нули:\n";
+    printArray(arr);
+
+    insertZeroRows(arr);
+    cout << "\nМассив после вставки строк из нулей перед строками с первым элементом, делящимся на 3:\n";
+    printArray(arr);
+
+    return EXIT_SUCCESS;
+}
+
+void checkN(const int n)
+{
+    if (n <= 0)
+    {
+        throw invalid_argument("Размер массива должен быть положительным числом");
+    }
+}
+
+size_t getSize()
+{
+    int n = 0;
+    cin >> n;
+    checkN(n);
+    return (size_t)n;
+}
+
+int getNumber()
+{
+    int number = 0;
+    cin >> number;
+    if (cin.fail())
+    {
+        cerr << "Неправильный ввод данных" << endl;
+        abort();
+    }
+    return number;
+}
+
+void printArray(const vector<vector<int>>& arr)
+{
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        for (size_t j = 0; j < arr[i].size(); j++)
+        {
+            cout << arr[i][j] << "\t";
         }
         cout << endl;
     }
-    cout << endl;
 }
 
-// Функция замены максимального элемента каждой строки на 0
-void replaceMaxInRows(int** matrix, int n, int m) {
-    for (int i = 0; i < n; ++i) {
-        if (m == 0) continue;
+void fillArrayRandom(vector<vector<int>>& arr, const int min, const int max)
+{
+    srand(static_cast<unsigned int>(time(0)));
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        for (size_t j = 0; j < arr[i].size(); j++)
+        {
+            arr[i][j] = rand() % (max - min + 1) + min;
+        }
+    }
+}
+
+void checkRange(const int min, const int max)
+{
+    if (min > max)
+    {
+        cerr << "Введен неправильный диапазон" << endl;
+        abort();
+    }
+}
+
+void fillArray(vector<vector<int>>& arr, const int min, const int max)
+{
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        for (size_t j = 0; j < arr[i].size(); j++)
+        {
+            cout << "Введите значение для arr[" << i << "][" << j << "]: ";
+            arr[i][j] = getNumber();
+
+            while (arr[i][j] < min || arr[i][j] > max)
+            {
+                cout << "Значение вне диапазона [" << min << ", " << max << "]. Повторите ввод: ";
+                arr[i][j] = getNumber();
+            }
+        }
+    }
+}
+
+void replaceMaxWithZero(vector<vector<int>>& arr)
+{
+    for (size_t i = 0; i < arr.size(); i++)
+    {
         int maxIndex = 0;
-        for (int j = 1; j < m; ++j) {
-            if (matrix[i][j] > matrix[i][maxIndex]) maxIndex = j;
+        for (size_t j = 1; j < arr[i].size(); j++)
+        {
+            if (arr[i][j] > arr[i][maxIndex])
+            {
+                maxIndex = j;
+            }
         }
-        matrix[i][maxIndex] = 0;
+        arr[i][maxIndex] = 0;
     }
 }
 
-// Функция вставки строк из нулей перед строками, первый элемент которых делится на 3
-int** insertZeroRows(int** matrix, int& n, int m) {
-    int zeroRows = 0;
-    for (int i = 0; i < n; ++i) {
-        if (matrix[i][0] % 3 == 0) {
-            ++zeroRows;
-        }
+void insertZeroRows(vector<vector<int>>& arr)
+{
+    vector<vector<int>> newArr;
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        if (arr[i].size() > 0 && arr[i][0] % 3 == 0)
+        {
+            newArr.push_back(vector<int>(arr[i].size(), 0));
+        }r.push_back(arr[i]);
     }
-
-    int newN = n + zeroRows;
-    int** newMatrix = (int**)malloc(newN * sizeof(int*));
-    if (!newMatrix) {
-        cerr << "Ошибка выделения памяти для новой матрицы!" << endl;
-        exit(1);
-    }
-
-    for (int i = 0; i < newN; ++i) {
-        newMatrix[i] = (int*)malloc(m * sizeof(int));
-        if (!newMatrix[i]) {
-            cerr << "Ошибка выделения памяти для строки матрицы!" << endl;
-            // Освободить ранее выделенную память
-            for (int j = 0; j < i; ++j) {
-                free(newMatrix[j]);
-            }
-            free(newMatrix);
-            exit(1);
-        }
-    }
-
-    int newRowIndex = 0;
-    for (int i = 0; i < n; ++i) {
-        if (matrix[i][0] % 3 == 0) {
-            // Вставить нулевую строку
-            for (int j = 0; j < m; ++j) {
-                newMatrix[newRowIndex][j] = 0;
-            }
-            ++newRowIndex;
-        }
-        // Скопировать исходную строку
-        for (int j = 0; j < m; ++j) {
-            newMatrix[newRowIndex][j] = matrix[i][j];
-        }
-        ++newRowIndex;
-    }
-
-    // Освободить память, занимаемую исходной матрицей
-    for (int i = 0; i < n; ++i) {
-        free(matrix[i]);
-    }
-    free(matrix);
-
-    n = newN;
-    return newMatrix;
-}
-
-int main() {
-    setlocale(LC_ALL, "rus");
-    srand(static_cast<unsigned int>(time(nullptr)));
-
-    int n = 0, m = 0;
-    cout << "Введите количество строк n: ";
-    cin >> n;
-    cout << "Введите количество столбцов m: ";
-    cin >> m;
-
-    // Выделение памяти для матрицы
-    int** matrix = (int**)malloc(n * sizeof(int*));
-    if (!matrix) {
-        cerr << "Ошибка выделения памяти для матрицы!" << endl;
-        return 1;
-    }
-
-    for (int i = 0; i < n; ++i) {
-        matrix[i] = (int*)malloc(m * sizeof(int));
-        if (!matrix[i]) {
-            cerr << "Ошибка выделения памяти для строки матрицы!" << endl;
-            // Освободить ранее выделенную память
-            for (int j = 0; j < i; ++j) {
-                free(matrix[j]);
-            }
-            free(matrix);
-            return 1;
-        }
-    }
-
-    // Заполнение массива случайными числами от 1 до 20
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            matrix[i][j] = rand() % 20 + 1;
-        }
-    }
-
-    cout << "Исходный массив:\n";
-    printMatrix(matrix, n, m);
-
-    replaceMaxInRows(matrix, n, m);
-
-    cout << "После замены максимального элемента каждой строки на 0:\n";
-    printMatrix(matrix, n, m);
-
-    matrix = insertZeroRows(matrix, n, m);
-
-    cout << "После вставки строк из нулей перед строками, первый элемент которых делится на 3:\n";
-    printMatrix(matrix, n, m);
-
-    // Освобождение памяти
-    for (int i = 0; i < n; ++i) {
-        free(matrix[i]);
-    }
-    free(matrix);
-
-    return 0;
-}
+    arr = newArr;
+} newAr
