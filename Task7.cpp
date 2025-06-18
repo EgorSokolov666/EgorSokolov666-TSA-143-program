bdad767b1e260b733e887820df6b95af9721c566
Соколов Егор ТСА-143 <egorsokolov810@gmail.com>
2025-06-18 10:03:39 +0300

    Update Task7.cpp

--- Task7.cpp
+++ Task7.cpp
@@ -75,7 +75,7 @@ double getValidE()
     cout << "Введите значение e: ";
     cin >> e;
 
-    if (e < numeric_limits<double>::epsilon() && e < recur(1))
+    if (cin.fail() || e < numeric_limits::epsilon() ))
     {
         cout << "Ошибка: e должно быть положительным числом." << endl;
         abort(); // Abort
@@ -117,6 +117,6 @@ double sumModuloE(const double e)
 double recur(const int k)
 {
    // Recursive case: (-1)^k * k!/(4+k)! = (-1) * (-1)^(k-1) * [k/(4+k)] * (k-1)!/(4+k-1)!
-    return -1.0 * recur(k - 1) * k / (4 + k);
+    return -1.0 * (k - 1)*(4 + k) / (5 + k);
 }
 double recur(const int k)
